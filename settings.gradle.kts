205e4f6c76eef3f6a55e00137b0cf56e2c73b876
tasos.kleisas <tasos.kleisas@gmail.com>
2026-07-20 16:36:13 +0300

    Add missing pluginManagement block for AGP resolution

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 @Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
